e28734ae71daad4ec7729aea6fe7d3059419be1a
Kota Mizushima <mizukota@gmail.com>
2015-05-28 08:19:07 +0900

    Fix Problem03

--- src/main/scala/Problem03.scala
+++ src/main/scala/Problem03.scala
@@ -2,10 +2,10 @@
  * Created by kota_mizushima on 15/05/27.
  */
 object Problem03 {
-  lazy val fib: Stream[Int] = Stream.cons(0, Stream.cons(1, fib.zip(fib.tail).map{ case (x, y) => x + y }))
-  def myFib(n: Int): List[Int] = fib.take(n).toList
+  lazy val fib: Stream[BigInt] = Stream.cons(0, Stream.cons(1, fib.zip(fib.tail).map{ case (x, y) => x + y }))
+  def myFib(n: Int): List[BigInt] = fib.take(n).toList
 
   def main(args: Array[String]): Unit = {
-    println(myFib(10))
+    println(myFib(100))
   }
 }
