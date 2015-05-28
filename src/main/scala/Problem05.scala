a7a6d3209c4189422ae1a610999c531403da8b17
Kota Mizushima <mizukota@gmail.com>
2015-05-28 12:45:39 +0900

    Problem05 naive

--- src/main/scala/Problem05.scala
+++ src/main/scala/Problem05.scala
@@ -23,32 +23,33 @@ object Problem05 {
   }
 
 
+  def compress(list: List[Group], previous: List[Group]): List[Group] = list match {
+    case Nil => previous.reverse
+    case y::ys =>
+      if(previous == Nil) {
+        compress(ys, List(y))
+      } else {
+        (y, previous) match {
+          case (PlusGroup(es), PlusGroup(ps)::px) =>
+            compress(ys, PlusGroup(ps ++ es)::px)
+          case (MinusGroup(es), MinusGroup(ps)::px) =>
+            compress(ys, MinusGroup(ps ++ es)::px)
+          case (NoneGroup(es), NoneGroup(ps)::px) =>
+            compress(ys, NoneGroup(ps ++ es)::px)
+          case (g, h) =>
+            compress(ys, g::h)
+        }
+      }
+  }
+
   def combination(list: List[Int]): List[List[Group]] = list match {
     case x::Nil => List(List(PlusGroup(List(x))), List(MinusGroup(List(x))), List(NoneGroup(List(x))))
     case x::xs =>
-      def compress(list: List[Group], previous: List[Group]): List[Group] = list match {
-        case Nil => previous
-        case y::ys =>
-          if(previous == Nil) {
-            compress(ys, List(y))
-          } else {
-            (y, previous) match {
-              case (PlusGroup(es), PlusGroup(ps)::px) =>
-                compress(ys, PlusGroup(ps ++ es)::px)
-              case (MinusGroup(es), MinusGroup(ps)::px) =>
-                compress(ys, MinusGroup(ps ++ es)::px)
-              case (NoneGroup(es), NoneGroup(ps)::px) =>
-                compress(ys, NoneGroup(ps ++ es)::px)
-              case (g, h) =>
-                compress(ys, g::h)
-            }
-          }
-      }
       val groups: List[Group] = List(PlusGroup(List(x)), MinusGroup(List(x)), NoneGroup(List(x)))
       groups.flatMap{g => combination(xs).map{s => g::s}}.map{xs => compress(xs, Nil)}
   }
 
   def main(args: Array[String]): Unit = {
-    println(combination((1 to 9).toList).filter{x => sum(x) == 100}.map{x => toStrings(x)}.map(x => x.mkString("+")))
+    println(Set(combination((1 to 9).toList).filter{x => sum(x) == 100}:_*).map{x => toStrings(x)}.map(x => x.mkString("+")))
   }
 }
