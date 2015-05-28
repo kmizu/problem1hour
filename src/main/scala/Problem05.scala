4feb95d90688eff4362aa7b5bc60b68da2d2fbba
Kota Mizushima <mizukota@gmail.com>
2015-05-28 11:44:38 +0900

    Answer of problem 5

--- src/main/scala/Problem05.scala
+++ src/main/scala/Problem05.scala
@@ -15,24 +15,40 @@ object Problem05 {
     case NoneGroup(es)::xs => es.map(_.toString).foldLeft("")(_ + _).toInt + sum(xs)
   }
 
-  def combination(list: List[Int]): List[List[Group]] = list match {
-    case Nil => List(List(NoneGroup(Nil)))
-    case x::xs =>
-      def compress(list: List[Group], previous: List[Group]): List[List[Group]] = list match {
-        case Nil => previous::Nil
-        case x::xs =>
-          Nil
-          /*
-          xs match {
-            case Nil => compress(xs, List(x))
-            case a@(PlusGroup(as))::xs =>
-          }
-          */
-      }
-      val groups: List[Group] = List(PlusGroup(List(x)), MinusGroup(List(x)), NoneGroup(List(x)))
-      groups.map{g => List(g) :: combination(xs)}
-
-      Nil
+  def toStrings(groups: List[Group]): List[String] = groups match {
+    case Nil => Nil
+    case PlusGroup(es)::xs => es.mkString("+") :: toStrings(xs)
+    case MinusGroup(es)::xs => ("-" + es.mkString("(", "+", ")")) :: toStrings(xs)
+    case NoneGroup(es)::xs => es.map(_.toString).foldLeft("")(_ + _) :: toStrings(xs)
   }
 
+
+  def combination(list: List[Int]): List[List[Group]] = list match {
+    case x::Nil => List(List(PlusGroup(List(x))), List(MinusGroup(List(x))), List(NoneGroup(List(x))))
+    case x::xs =>
+      def compress(list: List[Group], previous: List[Group]): List[Group] = list match {
+        case Nil => previous
+        case y::ys =>
+          if(previous == Nil) {
+            compress(ys, List(y))
+          } else {
+            (y, previous) match {
+              case (PlusGroup(es), PlusGroup(ps)::px) =>
+                compress(ys, PlusGroup(ps ++ es)::px)
+              case (MinusGroup(es), MinusGroup(ps)::px) =>
+                compress(ys, MinusGroup(ps ++ es)::px)
+              case (NoneGroup(es), NoneGroup(ps)::px) =>
+                compress(ys, NoneGroup(ps ++ es)::px)
+              case (g, h) =>
+                compress(ys, g::h)
+            }
+          }
+      }
+      val groups: List[Group] = List(PlusGroup(List(x)), MinusGroup(List(x)), NoneGroup(List(x)))
+      groups.flatMap{g => combination(xs).map{s => g::s}}.map{xs => compress(xs, Nil)}
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(combination(List(1, 2, 3)).filter{x => sum(x) == 6}.map{x => toStrings(x)}.map(x => x.mkString("+")))
+  }
 }
